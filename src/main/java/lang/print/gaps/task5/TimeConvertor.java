102501770cd977ce0d42ec3a245682f5b90ba705
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-09-26 22:30:11 +0500

    1
    ...

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float min = 10;
+        float sec = min*60;
+        System.out.println(sec);
     }
 }
